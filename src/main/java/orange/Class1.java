dc1555067ccae2efa46f242deec53bd1a86bf073
Lab <Lab@QAP>
2017-11-27 19:29:54 +0530

    asdf

---
+++ src/main/java/orange/Class1.java
@@ -0,0 +1,16 @@
+package orange;
+
+
+
+public class Class1 {
+//@Test
+	public  void haha() {
+	
+	System.out.println("Antony");
+		System.setProperty("webdriver.gecko.driver", "D:\\tet\\Selenium_Demp\\geckodriver.exe");
+		//WebDriver driver=new FirefoxDriver();
+		//driver.get("http://newtours.demoaut.com");
+
+	}
+
+}
