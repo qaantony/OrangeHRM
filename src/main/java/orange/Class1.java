0a401c8f9c242c24200f752007cb2e9c8cbc58e7
Lab <Lab@QAP>
2017-11-28 19:53:57 +0530

    wer

--- src/main/java/orange/Class1.java
+++ src/main/java/orange/Class1.java
@@ -1,15 +1,23 @@
 package orange;
 
-
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Class1 {
-//@Test
+@Test
 	public  void haha() {
 	
 	System.out.println("Antony");
 		System.setProperty("webdriver.gecko.driver", "D:\\tet\\Selenium_Demp\\geckodriver.exe");
-		//WebDriver driver=new FirefoxDriver();
-		//driver.get("http://newtours.demoaut.com");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://newtours.demoaut.com");
+		driver.quit();
+		
+		System.setProperty("webdriver.gecko.driver", "D:\\tet\\Selenium_Demp\\geckodriver.exe");
+		WebDriver driver2=new FirefoxDriver();
+		driver2.get("http://newtours.demoaut.com");
+		driver2.quit();
 
 	}
 
